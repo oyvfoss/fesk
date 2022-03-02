Worked on netCDF output functionality, mainly. Works now but needs cleanup and lookover. Also fixed a bug in the vector rotation function.

src/adcpyproc/_data_operation_methods_rdi_adcp.py
@@ -53,6 +53,7 @@ def _setup(self, matfn):
         self.u.mask.sum(), 100 * self.u.mask.sum()
         / (self.Nt * self.Ndep)))
 
+
     # Calculate time
     self._calculate_time(m)
 
@@ -62,7 +63,9 @@ def _setup(self, matfn):
 
     print('Calculating bin depths..')
     self._calculate_bin_depths(m)
+
     self.dep_med = np.median((self.dep), axis=1)
+
 
     # Flipping depth dimension if orientation is downward
     self._flip_to_ori()
@@ -135,7 +138,7 @@ def _rotate_uv(self, ang_deg, write_to_proc_dict=True):
     an offset to *direction*.
     """
     uvc_orig = self.u + 1j * self.v
-    uvc_rot = uvc_orig * np.exp(1j * ang_deg)
+    uvc_rot = uvc_orig * np.exp(1j * ang_deg*np.pi/180)
     self.u, self.v = uvc_rot.real, uvc_rot.imag
     self.direction = self.direction + ang_deg
     if hasattr(self, 'shu'):

src/adcpyproc/_read_rdifile_methods_rdi_adcp.py
@@ -29,8 +29,8 @@ def _load_data_from_matfile(self, mfile, vardict, explicit = False):
                 elif vd_['dim'] == 1:
                     outvar_ = mfile[nm] * vd_['fact_in_to_out']
                     n_1dvars += 1
-            except Exception:
-                Warning('Issue with variable "%s"/"%s"' % (
+            except:
+                raise Warning('Issue with variable "%s"/"%s"' % (
                     outnm_, nm) +
                     '(skipping this variable).')
 
@@ -42,6 +42,7 @@ def _load_data_from_matfile(self, mfile, vardict, explicit = False):
 
     print('Reading data arrays: -> done.         '
             '(%i 2D variables)' % (n_2dvars + n_1dvars))
+
     for nm in ('u', 'v', 'w', 'direction', 'errvel'):
         try:
             outvar_masked_ = np.ma.masked_invalid(getattr(self, nm))
@@ -51,6 +52,7 @@ def _load_data_from_matfile(self, mfile, vardict, explicit = False):
             pass
 
 #######################################################################
+
 
 def _get_orientation(self):
     '''
@@ -97,11 +99,11 @@ def _calculate_bin_depths(self, mfile):
     elif self_ori == 'down':
         sign = +1
 
-    self.dep = (mfile['AnDepthmm'][np.newaxis, :] * 0.001 
+    self.dep = (self.tdepth[np.newaxis, :]
                 + sign * mfile['RDIBin1Mid'] + sign * mfile['RDIBinSize'] 
-                * (mfile['SerBins'][:, np.newaxis] - 1))[0].T
+                * (mfile['SerBins'][0] - 1)[:, np.newaxis])
     self.units['dep'] = 'm'
-    self.bin_size = mfile['RDIBinSize'] 
+    self.bin_size = mfile['RDIBinSize'][0][0]
     self.var_desc['dep'] = 'Time-varying depth of bin (m)'
 
 #######################################################################
@@ -152,4 +154,6 @@ def _calculate_time(self, mfile):
                                 '(days since 1970-1-1)')
     self.var_desc['t_datetime'] = ('Time stamp (matplotlib '
                                     'datetime object)')
+    self.d_t = np.median(np.diff(t_mpl))
+    
     print('Calculating time: -> done.          ')